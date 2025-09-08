e41b60faca4e3a0a1c7ca5499b13a2a4ec41466a
mohammadrayati <mohammad.rayiati@gmail.com>
2025-09-08 11:43:17 +0200

    add GFM inverter examples

--- api/app/power_flow_opendss.py
+++ api/app/power_flow_opendss.py
@@ -7,14 +7,9 @@ print("OpenDSSDirect.py and engine versions:", dss.Version())
 dss.Basic.ClearAll()
 file_path = (
     os.path.dirname(os.path.abspath(__file__))
-    + "/../../data/inputs/European_LV_Test_Feeder-G/Master.dss"
+    + "/../../data/inputs/GFL_IEEE123/Run_IEEE123Bus_GFLDaily_DynExp.dss"
 )
 
 os.chdir(os.path.dirname(file_path))
+dss.Basic.ClearAll()
 dss.Command(f'Redirect "{os.path.abspath(file_path)}"')
-dss.Solution.Mode(enums.SolveModes.Daily)
-
-
-print(dss.utils.loads_to_dataframe())
-
-print(dss.utils.monitors_to_dataframe())
