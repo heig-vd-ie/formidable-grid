703bc75e40636119c2d549b0e053fa5294cb2891
mohammadrayati <mohammad.rayiati@gmail.com>
2025-09-23 09:17:36 +0200

    Refactor Ray API routes and improve initialization response

--- api/main.py
+++ api/main.py
@@ -1,42 +1,33 @@
-import time
-
 from flask import Flask
 from flask_restx import Api, Resource
 import ray
+from ray._private.worker import BaseContext
 
 from common.konfig import SECRET_KEY
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
-api = Api(app, doc="/docs")  # /docs will show Swagger UI
+api = Api(app, doc="/")  # / will show Swagger UI
 
-ns = api.namespace("ray", description="Ray Operations")
-
-
-@app.route("/")
-def index():
-    return "Hello, World!"
+ns_ray = api.namespace("RAY", description="Ray Operations")
 
 
-@app.route("/api/time")
-def get_current_time():
-    return {"time": time.time()}
-
-
-@ns.route("/api/ray-init")
+@ns_ray.route("/ray-init")
 class RayInit(Resource):
     def get(self):
         if not ray.is_initialized():
-            context = ray.init()
-            print(context.dashboard_url)
-            return_result = {"status": "Ray initialized"}
+            context: BaseContext = ray.init()
+            return_result = {
+                "status": "Ray initialized",
+                "dashboard_url": str(context.dashboard_url),
+                "ray_version": str(context.ray_version),
+            }
         else:
             return_result = {"status": "Ray already initialized"}
-
         return return_result
 
 
-@ns.route("/api/ray-shutdown")
+@ns_ray.route("/ray-shutdown")
 class RayShutdown(Resource):
     def get(self):
         if ray.is_initialized():
